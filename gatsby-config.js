5107691bc1da547eca7fa40847a25c458f97391b
krnblni <karanbalani007@gmail.com>
2020-05-07 01:44:11 +0530

    added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,17 @@ module.exports = {
         name: `data`,
         path: `${__dirname}/src/data/`,
       }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-144186313-1",
+        head: false,
+        anonymize: true,
+        respectDNT: true,
+        pageTransitionDelay: 0,
+        defer: false,
+      },
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
