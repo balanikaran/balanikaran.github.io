4511c31b1ba4a12253ab7f500a83248a521043d3
krnblni <karanbalani007@gmail.com>
2020-05-07 00:23:05 +0530

    added seo and staged for rollout to master branch

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ import Footer from "../components/footer"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="krnblni" />
     <Header/>
     <About/>
     <Projects/>
