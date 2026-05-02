9081b3e13f544f53bfe39311b34e0efe5668c5d3
Karan Balani <29383381+balanikaran@users.noreply.github.com>
2026-05-02 18:33:02 +0530

    refactor: drop Sass and inline icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 import { defineConfig } from "astro/config"
+import icon from "astro-icon"
 
 export default defineConfig({
   site: "https://karanbalani.com",
+  integrations: [
+    icon({
+      svgoOptions: {
+        plugins: [
+          { name: "preset-default" },
+          {
+            name: "prefixIds",
+            params: {
+              prefix: () =>
+                Math.round(Math.random() * 1_000_000_000).toString(36),
+            },
+          },
+        ],
+      },
+    }),
+  ],
 })
